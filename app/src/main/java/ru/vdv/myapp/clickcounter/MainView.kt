e08c0c09cb37c433a169f42ed186ecbb6afe7b8f
Dmitriy <stdimensiy@yandex.ru>
2021-08-01 00:45:12 +0300

    Изм 2.2. - задание выполнено. Пи повороте экрана состояние счетчиков сохраняется. все работает.

--- app/src/main/java/ru/vdv/myapp/clickcounter/MainView.kt
+++ app/src/main/java/ru/vdv/myapp/clickcounter/MainView.kt
@@ -1,6 +1,11 @@
 package ru.vdv.myapp.clickcounter
 
-interface MainView {
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface MainView : MvpView {
     fun setButtonOneText(text: String)
     fun setButtonTwoText(text: String)
     fun setButtonThreeText(text: String)
